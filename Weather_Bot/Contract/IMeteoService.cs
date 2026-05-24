0117b858735d77712d5c65be08b8711f6c229e58
Choffel <daniilmiadzve@gmail.com>
2026-05-25 01:15:01 +0300

    chore: recreate all contracts

---
+++ Weather_Bot/Contract/IMeteoService.cs
@@ -0,0 +1,9 @@
+﻿using Weather_Bot.DTOs.OpenMeteoDTOs;
+
+namespace Weather_Bot.Contract;
+
+public interface IMeteoService
+{
+    Task<OpenMeteoResponse?> GetWindAsync(double latitude, double longitude);
+    
+}
---
+++ Weather_Bot/Contract/ITelegramService.cs
@@ -0,0 +1,10 @@
+﻿namespace Weather_Bot.Contract;
+
+public interface ITelegramService
+{
+    Task StartAsync();
+    
+    Task GetWindASync(double latitude, double longitude);
+    
+    Task SendEveningReportAsync(double latitude, double longitude);
+}
