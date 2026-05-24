25bb11f3222fef0fea36117bf12f032561288ca9
Choffel <daniilmiadzve@gmail.com>
2026-05-25 01:15:29 +0300

    reaft: add new DTOs for services

---
+++ Weather_Bot/DTOs/OpenMeteoDTOs/CurrentWeatherData.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Weather_Bot.DTOs.OpenMeteoDTOs;
+
+public  class CurrentWeatherData
+{
+    [JsonPropertyName("time")]
+    public static string Time { get; set; }
+
+    // Скорость ветра
+    [JsonPropertyName("wind_speed_10m")]
+    public static  double WindSpeed { get; set; }
+
+    // Направление ветра в градусах (0-360)
+    [JsonPropertyName("wind_direction_10m")]
+    public static  int WindDirection { get; set; }
+
+    // Порывы ветра
+    [JsonPropertyName("wind_gusts_10m")]
+    public static double WindGusts { get; set; }
+}
---
+++ Weather_Bot/DTOs/OpenMeteoDTOs/OpenMeteoResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Weather_Bot.DTOs.OpenMeteoDTOs;
+
+public class OpenMeteoResponse
+{
+    [JsonPropertyName("latitude")]
+    public double Latitude { get; set; }
+
+    [JsonPropertyName("longitude")]
+    public double Longitude { get; set; }
+
+    [JsonPropertyName("current")]
+    public CurrentWeatherData Current { get; set; }
+}
