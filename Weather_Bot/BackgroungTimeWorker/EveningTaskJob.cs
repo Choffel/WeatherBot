cb697ad39f9f77303b64b59ae8e7ce0b52395606
Choffel <daniilmiadzve@gmail.com>
2026-05-25 01:14:32 +0300

    feat: add background worker

---
+++ Weather_Bot/BackgroungTimeWorker/EveningTaskJob.cs
@@ -0,0 +1,19 @@
+﻿using Quartz;
+using Weather_Bot.Contract;
+
+namespace Weather_Bot.BackgroungTimeWorker;
+
+public class EveningTaskJob : IJob
+{
+    private readonly  IMeteoService _meteoService;
+    
+    public EveningTaskJob(IMeteoService meteoService)
+    {
+        _meteoService = meteoService;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        await _messageSender.SendEveningReportAsync();
+    }
+}
