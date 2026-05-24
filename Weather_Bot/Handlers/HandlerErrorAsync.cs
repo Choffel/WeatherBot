467cd3f0e537ada0edf4cdf52162026ad01614c6
Choffel <daniilmiadzve@gmail.com>
2026-05-25 01:16:13 +0300

    refactor: relocation telegram handlers

---
+++ Weather_Bot/Handlers/HandlerErrorAsync.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot;
+
+namespace Weather_Bot.Handlers;
+
+public class HandlerErrorAsync
+{
+    public  async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        // Log the error (you can use any logging framework you prefer)
+        Console.WriteLine($"Error occurred: {exception.Message}");
+        
+        // Optionally, you can send a message to the user about the error
+        // await botClient.SendTextMessageAsync(chatId: <user_chat_id>, text: "An error occurred while processing your request. Please try again later.", cancellationToken: cancellationToken);
+    }
+}
---
+++ Weather_Bot/Handlers/HandlerUpdateAsync.cs
@@ -0,0 +1,30 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Weather_Bot.Contract;
+
+namespace Weather_Bot.Handlers;
+
+public class HandlerUpdateAsync
+{
+    private readonly ITelegramService _telegramService;
+
+    public HandlerUpdateAsync(ITelegramService telegramService)
+    {
+        _telegramService = telegramService;
+    }
+    
+       public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+       {
+           if (update.Message?.Text == "/start")
+           {
+               await _telegramService.StartAsync();
+           }
+           else if (update.Message?.Text == "/wind")
+           {
+               double latitude = double.Parse(Environment.GetEnvironmentVariable("LATITUDE") ?? "-58.4");
+               double longitude = double.Parse(Environment.GetEnvironmentVariable("LONGITUDE") ?? "-62.8");
+                
+               await _telegramService.GetWindASync(latitude, longitude);
+           }
+       }
+}
