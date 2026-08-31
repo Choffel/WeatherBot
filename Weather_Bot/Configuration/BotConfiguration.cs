984125557606987c09772531a3d12be070020151
Daniil <71185355+Choffel@users.noreply.github.com>
2026-08-31 18:27:11 +0200

    Update bot config

--- Weather_Bot/Configuration/BotConfiguration.cs
+++ Weather_Bot/Configuration/BotConfiguration.cs
@@ -4,7 +4,7 @@
     {
         public const string SectionName = "BotConfiguration";
 
-        public string TELEGRAM_BOT_TOKEN { get; set; } = string.Empty;
+        public string TELEGRAM_BOT_TOKEN { get; set; } 
         public long TELEGRAM_CHAT_ID { get; set; }
         public double LATITUDE { get; set; }
         public double LONGITUDE { get; set; }
