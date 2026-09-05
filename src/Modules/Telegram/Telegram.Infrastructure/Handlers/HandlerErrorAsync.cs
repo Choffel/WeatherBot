4f57a5979bdba4f38cf1d6ea1263317612070154
Daniil <71185355+Choffel@users.noreply.github.com>
2026-09-05 02:20:40 +0200

    refactor(telegram):start refactoring telegram-handlers

---
+++ src/Modules/Telegram/Telegram.Infrastructure/Handlers/HandlerErrorAsync.cs
@@ -0,0 +1,16 @@
+﻿using Telegram.Bot;
+
+namespace Telegram.Infrastructure.Handlers;
+
+//TODO: To change
+public class HandlerErrorAsync
+{
+    public  Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        if (exception.InnerException != null)
+        {
+            Console.WriteLine($"Inner Exception: {exception.InnerException.Message}");
+        }
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Modules/Telegram/Telegram.Infrastructure/Handlers/HandlerUpdateAsync.cs
@@ -0,0 +1,30 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Telegram.Infrastructure.Handlers;
+
+public class HandlerUpdateAsync
+{
+    private readonly ITelegramBotClient _botClient;
+    
+    public HandlerUpdateAsync(ITelegramBotClient botClient)
+    {
+        _botClient = botClient;
+    }
+    
+    public async Task HandleUpdateAsync(ITelegramBotClient botClient,Update update, CancellationToken ct)
+    {
+        var message = update.Message;
+        var text = message.Text;
+
+        if (text == null)
+        {
+            await _botClient.SendMessage(message.Chat.Id, "Please send a text message.", cancellationToken: ct);
+        }
+        
+        if(text == "/start")
+        {
+            await _botClient.SendMessage(message.Chat.Id, " write the city", cancellationToken: ct);
+        }
+    }
+}
