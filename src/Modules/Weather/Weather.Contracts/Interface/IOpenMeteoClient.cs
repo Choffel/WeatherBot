613b4889860d73eb77e7a22fc39c75b0e1296b03
Daniil <71185355+Choffel@users.noreply.github.com>
2026-09-05 02:22:51 +0200

    feat(weather): add OpenMeteoClient and its implementation

---
+++ src/Modules/Weather/Weather.Contracts/Interface/IOpenMeteoClient.cs
@@ -0,0 +1,8 @@
+﻿namespace Weather.Contracts.Interface;
+
+public interface IOpenMeteoClient
+{
+    Task<(double latitude, double longitude, string? cityName)> GetCityCoordinatesAsync(double lat, double lon, string city);
+    
+    Task GetWeatherAsync(double latitude, double longitude);
+}
---
+++ src/Modules/Weather/Weather.Infrastructure/Services/OpenMeteoClient.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using Weather.Contracts.Interface;
+
+namespace Weather.Infrastructure.Services;
+
+public class OpenMeteoClient : IOpenMeteoClient
+{
+    private static readonly HttpClient _httpClient = new HttpClient();
+    
+    public  async Task<(double latitude, double longitude, string? cityName)>GetCityCoordinatesAsync (double lat, double lon,
+        string city)
+    {
+        string safeCity = Uri.EscapeDataString(city.Trim());
+        
+        string url = $"https://geocoding-api.open-meteo.com/v1/search?name={safeCity}&count=1&language=ru&format=json";
+        
+        using var response = await _httpClient.GetAsync(url);
+        
+        string json = await response.Content.ReadAsStringAsync();
+        
+        using var doc = JsonDocument.Parse(json);
+
+        if (!doc.RootElement.TryGetProperty("results", out var results) || results.GetArrayLength() == 0)
+        {
+            return (lat, lon, city);
+        }
+        
+        var firstResult = results[0];
+        double latitude = firstResult.GetProperty("latitude").GetDouble();
+        double longitude = firstResult.GetProperty("longitude").GetDouble();
+        string cityName = firstResult.GetProperty("name").GetString();
+        
+        return (latitude, longitude, cityName);
+    }
+
+    public async Task GetWeatherAsync(double lat, double lon)
+    {
+        string latStr = lat.ToString(System.Globalization.CultureInfo.InvariantCulture);
+        string lonStr = lon.ToString(System.Globalization.CultureInfo.InvariantCulture);
+
+        string url = $"https://api.open-meteo.com/v1/forecast?latitude={latStr}&longitude={lonStr}&current=temperature_2m,wind_speed_10m,wind_gusts_10m&wind_speed_unit=ms";
+
+        using var response = await _httpClient.GetAsync(url);
+         
+        await response.Content.ReadAsStringAsync();
+    }
+}
