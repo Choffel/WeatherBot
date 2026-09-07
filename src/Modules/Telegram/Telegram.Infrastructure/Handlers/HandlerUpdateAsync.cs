b1cf9d92f7dc82ced529a677bd6973af922a5306
Daniil <71185355+Choffel@users.noreply.github.com>
2026-09-07 03:58:56 +0200

    refactor: update telegram HandlerUpdateAsync.cs

--- src/Modules/Telegram/Telegram.Infrastructure/Handlers/HandlerUpdateAsync.cs
+++ src/Modules/Telegram/Telegram.Infrastructure/Handlers/HandlerUpdateAsync.cs
@@ -1,30 +1,53 @@
-﻿using Telegram.Bot;
+﻿using System.Collections.Concurrent;
+using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Weather.Contracts.Interface;
 
 namespace Telegram.Infrastructure.Handlers;
 
 public class HandlerUpdateAsync
 {
-    private readonly ITelegramBotClient _botClient;
-    
-    public HandlerUpdateAsync(ITelegramBotClient botClient)
-    {
-        _botClient = botClient;
-    }
-    
-    public async Task HandleUpdateAsync(ITelegramBotClient botClient,Update update, CancellationToken ct)
-    {
-        var message = update.Message;
-        var text = message.Text;
+    private readonly ITelegramBotClient _bot;
+    private readonly IOpenMeteoClient _openMeteoClient;
 
-        if (text == null)
+    
+    private static readonly ConcurrentDictionary<long, bool> WaitingCity = new();
+
+    public HandlerUpdateAsync(ITelegramBotClient bot, IOpenMeteoClient openMeteoClient)
+    {
+        _bot = bot;
+        _openMeteoClient = openMeteoClient;
+    }
+
+    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken ct)
+    {
+        if (update.Type != UpdateType.Message || update.Message?.Text is null)
+            return;
+
+        var chatId = update.Message.Chat.Id;
+        var text = update.Message.Text.Trim();
+
+        if (text.Equals("/start", StringComparison.OrdinalIgnoreCase))
         {
-            await _botClient.SendMessage(message.Chat.Id, "Please send a text message.", cancellationToken: ct);
+            WaitingCity[chatId] = true;
+            await _bot.SendMessage(chatId, "Введите город", cancellationToken: ct);
+            return;
         }
+
+        if (!WaitingCity.ContainsKey(chatId))
+            return;
         
-        if(text == "/start")
-        {
-            await _botClient.SendMessage(message.Chat.Id, " write the city", cancellationToken: ct);
-        }
+        var (lat, lon, cityName) = await _openMeteoClient.GetCityCoordinatesAsync(0, 0, text);
+        
+        //must be awaited Task<string> in interface and implementation
+        var weatherSummary =  _openMeteoClient.GetWeatherAsync(lat, lon);
+
+        WaitingCity.TryRemove(chatId, out _);
+
+        await _bot.SendMessage(
+            chatId,
+            $"Погода для {cityName ?? text}:\n{weatherSummary}",
+            cancellationToken: ct);
     }
 }
