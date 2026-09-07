ed375ddba11b6ac804dd9797c414f19b42d43f3a
Daniil <71185355+Choffel@users.noreply.github.com>
2026-09-07 03:58:11 +0200

    refactor: register services to di

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -1,6 +1,24 @@
+using Telegram.Bot;
+using Telegram.Contracts;
+using Telegram.Infrastructure.Handlers;
+using Telegram.Infrastructure.Service;
+using Weather.Contracts.Interface;
+using Weather.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<ITelegramBotClient>(_ =>
+    new TelegramBotClient(
+        Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN")
+        ?? builder.Configuration["TELEGRAM_BOT_TOKEN"]
+        ?? throw new InvalidOperationException("TELEGRAM_BOT_TOKEN is missing.")));
+
+builder.Services.AddSingleton<IOpenMeteoClient, OpenMeteoClient>();
+builder.Services.AddSingleton<HandlerUpdateAsync>();
+builder.Services.AddSingleton<HandlerErrorAsync>();
+builder.Services.AddSingleton<ITelegramService, TelegramService>();
 
 var app = builder.Build();
 
